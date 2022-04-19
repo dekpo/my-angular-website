d31287fec5cdf6ef84456581f3c6afd78712c213
Dekpo-Wyna Yologaza <dw.yologaza@yahoo.fr>
2022-04-19 12:39:31 +0200

    Added some few changes to portfolio...

--- src/app/portfolio/portfolio.component.ts
+++ src/app/portfolio/portfolio.component.ts
@@ -12,22 +12,56 @@ export class PortfolioComponent implements OnInit {
 
   joke:any;
   jokeNotFound = true;
-
   gallery:any;
+  page = 1;
+  urlPicsum = 'https://picsum.photos/v2/list?page='+this.page+'&limit=6';
+  urlChuck = 'https://api.chucknorris.io/jokes/random';
+
+  prevPage(){
+    this.page>1 ? this.page-- : null;
+    this.urlPicsum = 'https://picsum.photos/v2/list?page='+this.page+'&limit=6';
+    this.loadPics();
+    console.log(this.page);
+    console.log(this.urlPicsum);
+  }
+
+  goToPage(nb:number){
+    this.page = nb;
+    this.urlPicsum = 'https://picsum.photos/v2/list?page='+this.page+'&limit=6';
+    this.loadPics();
+    console.log(this.page);
+    console.log(this.urlPicsum);
+  }
+
+  nextPage(){
+    this.page = this.page + 1;
+    this.urlPicsum = 'https://picsum.photos/v2/list?page='+this.page+'&limit=6';
+    this.loadPics();
+    console.log(this.page);
+    console.log(this.urlPicsum);
+  }
+
+  getUrl(url:string){
+    return this.http.get(url);
+  }
+
+  loadPics(){
+    this.getUrl(this.urlPicsum).subscribe(
+      data => {
+        this.gallery = data;
+        console.log(data);
+      }
+    )
+  }
 
   ngOnInit(): void {
-    this.http.get('https://api.chucknorris.io/jokes/random').subscribe(
+    this.getUrl(this.urlChuck).subscribe(
       (data) => {
         this.joke = data;
         this.jokeNotFound = false;
         console.log(data);
       }
     )
-    this.http.get('https://picsum.photos/v2/list?page=2&limit=24').subscribe(
-      data => {
-        this.gallery = data;
-        console.log(data);
-      }
-    )
+    this.loadPics();
   }
 }
