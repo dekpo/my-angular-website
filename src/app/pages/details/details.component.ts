744cdedebf71e821480532d06b0b716164366950
Dekpo-Wyna Yologaza <dw.yologaza@yahoo.fr>
2022-04-20 11:48:57 +0200

    Added some blur logic to the details view

--- src/app/pages/details/details.component.ts
+++ src/app/pages/details/details.component.ts
@@ -15,21 +15,22 @@ export class DetailsComponent implements OnInit {
   itemInfo:any;
   imgSrc = '';
   gray = false;
+  blur = 0;
 
-  generateSrc(id=this.itemId,gray=false,blur=0){
-    this.imgSrc = 'https://picsum.photos/id/'+this.itemId+'/1280/960';
-    this.gray? this.imgSrc +='?grayscale' : null;
-    /* if (this.gray) {
-      this.imgSrc +='?grayscale';
-    } else {
-      null;
-    } */
+  generateSrc(){
+    this.imgSrc = 'https://picsum.photos/id/'+this.itemId+'/1280/960?';
+    this.gray? this.imgSrc +='grayscale&' : null;
+    this.blur>0? this.imgSrc += 'blur=' + this.blur : null;
   }
 
   grayscale(){
     this.gray=!this.gray;
     this.generateSrc();
-    console.log(this.gray);
+  }
+
+  addBlur(){
+    this.blur<10?this.blur++:this.blur=0;
+    this.generateSrc();
   }
 
   ngOnInit(): void {
@@ -40,7 +41,7 @@ export class DetailsComponent implements OnInit {
     this.http.get('https://picsum.photos/id/'+this.itemId+'/info').subscribe(
       data => {
         this.itemInfo = data;
-        this.generateSrc(this.itemId);
+        this.generateSrc();
         console.log(data); 
       }
     )
