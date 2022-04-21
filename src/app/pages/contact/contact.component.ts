2c67bbf195790625a21db4160a48c91a12003aec
Dekpo-Wyna Yologaza <dw.yologaza@yahoo.fr>
2022-04-21 17:03:43 +0200

    Added a form and made a new build

--- src/app/pages/contact/contact.component.ts
+++ src/app/pages/contact/contact.component.ts
@@ -8,7 +8,37 @@ import { SettingsService } from '../../services/settings.service';
 })
 export class ContactComponent implements OnInit {
 
-  constructor(public settings:SettingsService) { }
+  constructor(public settings: SettingsService) { }
+
+  firstname = '';
+  lastname = '';
+  email = '';
+  message = '';
+
+  onSubmit(formData: any) {
+    let score = 0;
+    let max = 3;
+    let errorMessage = '';
+
+    formData.firstname.length > 2 ? score++ : errorMessage += "Firstname is too short !\n";
+    formData.lastname.length > 2 ? score++ : errorMessage += "Lastname is too short !\n";
+    /* formData.email.indexOf('@') > 1 ? score++ : errorMessage += "Email must contain an @ !\n"; */
+    if (/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(formData.email)) {
+      score++ 
+    } else {
+      errorMessage += "Email must be a valid address !\n";
+    }
+
+    let validation = score === max ? true : false;
+
+    if (validation) {
+      console.log('Youpi ! Le formulaire est bien rempli !', formData);
+    } else {
+      alert('Oups an error occured !\n' + errorMessage);
+    }
+
+    console.log(formData);
+  }
 
   ngOnInit(): void {
     this.settings.displayCarousel = false;
