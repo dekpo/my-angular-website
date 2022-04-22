07950d80e7d028fc47a0650ba6be034de256e1bf
Dekpo-Wyna Yologaza <dw.yologaza@yahoo.fr>
2022-04-22 14:40:13 +0200

    Ran 'npm i @angular/service-worker --force' and 'ng add @angular/pwa --project my-angular-website' to add PWA to the app

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -15,6 +15,8 @@ import { DetailsComponent } from './pages/details/details.component';
 import { CarouselComponent } from './carousel/carousel.component';
 import { LoginComponent } from './pages/login/login.component';
 import { ThanksComponent } from './pages/thanks/thanks.component';
+import { ServiceWorkerModule } from '@angular/service-worker';
+import { environment } from '../environments/environment';
 
 @NgModule({
   declarations: [
@@ -35,7 +37,13 @@ import { ThanksComponent } from './pages/thanks/thanks.component';
     AppRoutingModule,
     HttpClientModule,
     FormsModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    ServiceWorkerModule.register('ngsw-worker.js', {
+      enabled: environment.production,
+      // Register the ServiceWorker as soon as the application is stable
+      // or after 30 seconds (whichever comes first).
+      registrationStrategy: 'registerWhenStable:30000'
+    })
   ],
   providers: [],
   bootstrap: [AppComponent]
