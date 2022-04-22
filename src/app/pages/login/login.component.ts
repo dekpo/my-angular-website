dcbc997c6ade8e0af87093122b6ba5d1f04b13d7
Dekpo-Wyna Yologaza <dw.yologaza@yahoo.fr>
2022-04-22 13:09:19 +0200

    Added some HTTP logic to the LoginForm component

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder,FormControl,FormGroup,Validators  } from '@angular/forms';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-login',
@@ -8,11 +9,31 @@ import { FormBuilder,FormControl,FormGroup,Validators  } from '@angular/forms';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    public formBuilder:FormBuilder,
+    public http:HttpClient) { }
 
-  
+  loginForm = new FormGroup({
+    user: new FormControl(''),
+    password: new FormControl('')
+  });
+
+  login(){
+    console.log(this.loginForm.value);
+
+    this.http.post("https://httpbin.org/post",this.loginForm.value).subscribe(
+      response => {
+        console.log("Woohoo le serveur nous répond !", response)
+      }
+    )
+    
+  }
 
   ngOnInit(): void {
+    this.loginForm = this.formBuilder.group({
+      user:['',[Validators.required,Validators.minLength(3)]],
+      password:['',[Validators.required,Validators.minLength(5)]]
+    })
   }
 
 }
